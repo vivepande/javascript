6bfc5be94e274a5842b1f0556ee73c6505b27696
vivepande <138912281+vivepande@users.noreply.github.com>
2024-03-22 18:00:00 +0000

    datatype

---
+++ datatype.js
@@ -0,0 +1,17 @@
+console.log("hello");
+
+/*data type in js
+primitive
+non primitive
+
+. primitive data type
+string=""
+bright
+number=4554
+null
+undefined
+boolean=true/false
+symbol
+
+*/
+console.log(typeof undefined );
